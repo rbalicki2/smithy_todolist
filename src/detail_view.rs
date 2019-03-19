2636961c241b171eeac32cfcc268368269f0f6e7
Robert Balicki <robert.balicki@gmail.com>
2019-03-19 09:42:25 -0400

    moar

--- src/detail_view.rs
+++ src/detail_view.rs
@@ -7,9 +7,16 @@ use smithy::{
   },
 };
 
-pub fn render_detail_view_page<'a>() -> SmithyComponent<'a> {
-  smd!(<h1>
-    Detail View
-  </h1>
-  <a href="#">Go Home</a>)
+pub fn render_detail_view_page<'a>(todo_lists: &'a mut Vec<TodoList>) -> SmithyComponent<'a> {
+  smd!(
+    <h1>
+      Detail View
+    </h1>
+    <a href="#">Go Home</a>
+    <ul>
+      {
+        todo_lists.iter().map(|_| smd!(<li>item</li>)).collect::<Vec<SmithyComponent>>()
+      }
+    </ul>
+  )
 }
